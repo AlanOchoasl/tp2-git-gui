57bb7cc9b47384fcfbf4cf2c31f5a9f28a53acb5
AlanOchoasl <ochoaalan2020@gmail.com>
2026-09-01 20:17:05 -0300

    JDesktop, JPanel y labels password

--- Tp2Laboratorio/src/tp2laboratorio/Tp2Laboratorio.java
+++ Tp2Laboratorio/src/tp2laboratorio/Tp2Laboratorio.java
@@ -1,19 +1,13 @@
 
 package tp2laboratorio;
 
-/**
- *
- * @author Charly Cimino
- * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
- * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
- */
+import GUI.Pantalla;
+
 public class Tp2Laboratorio {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
     }
 
 }
